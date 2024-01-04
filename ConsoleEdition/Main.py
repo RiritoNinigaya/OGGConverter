f31b87f632f258d81b5381ed2345be5574fdc6c8
Ririto Ninigaya <154670403+RiritoNinigaya@users.noreply.github.com>
2024-01-04 21:21:29 +0200

    Add files via upload

---
+++ ConsoleEdition/Main.py
@@ -0,0 +1,20 @@
+import ctypes
+import subprocess
+import os
+import tk
+import tkinter
+from tkinter import ttk, filedialog
+def AskMP3FileOpen():
+    str_mp3 = filedialog.askopenfile(mode="r", filetypes=[('MP3 File', '*.mp3')], title="Please Choose MP3 File to Convert OGG")
+    return os.path.abspath(str_mp3.name)
+
+def Main():
+    stringlit = AskMP3FileOpen()
+    oggname = input("Please Write OGG Name of You're Sound Or Music: ")
+    fh = open("NUL","w")
+    subprocess.Popen("ffmpeg -i {} -c:a libvorbis {}.ogg".format(stringlit, oggname), stdout = fh, stderr = fh)
+    fh.close()
+    os._exit(40403)
+
+if __name__ == "__main__":
+    Main()
