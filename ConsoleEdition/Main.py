ecbdc51e4a35a9bdc89ba6290b02128de56f9976
Ririto Ninigaya <154670403+RiritoNinigaya@users.noreply.github.com>
2024-01-07 00:44:46 +0200

    Added Support MP4 Files
    Added Support MP4 Files!!!

--- ConsoleEdition/Main.py
+++ ConsoleEdition/Main.py
@@ -5,14 +5,14 @@ import tk
 import tkinter
 from tkinter import ttk, filedialog
 def AskMP3FileOpen():
-    str_mp3 = filedialog.askopenfile(mode="r", filetypes=[('MP3 File', '*.mp3')], title="Please Choose MP3 File to Convert OGG")
+    str_mp3 = filedialog.askopenfile(mode="r", filetypes=[('MP3 File', '*.mp3'), ('MP4 File', '*.mp4')], title="Please Choose MP3 File to Convert OGG")
     return os.path.abspath(str_mp3.name)
 
 def Main():
     stringlit = AskMP3FileOpen()
-    oggname = input("Please Write OGG Name for Saving You're File: ")
+    oggname = input("Please Write OGG Name of You're Sound Or Music: ")
     fh = open("NUL","w")
-    subprocess.Popen("ffmpeg -i {} -c:a libvorbis {}.ogg".format(stringlit, oggname), stdout = fh, stderr = fh)
+    subprocess.Popen("ffmpeg -i {} -c:a libvorbis {}.ogg".format(stringlit, oggname), stdout = open("output.txt", "w"), stderr = fh)
     fh.close()
     os._exit(40403)
 
